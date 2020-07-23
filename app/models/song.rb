c4ac8cb04708180fae5fbef872dd19b1d9fda2fd
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 01:53:22 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    self.artist ? self.artist.name : nil
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
