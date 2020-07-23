47b7bad262a539f49ee771c7d586664cf156beb5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 01:50:36 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist_name
+      link_to song.artist_name, artist_path(song.artist_id)
+    else
+      link_to "Add Artist", edit_song_path(song.id)
+    end
+  end
 end
