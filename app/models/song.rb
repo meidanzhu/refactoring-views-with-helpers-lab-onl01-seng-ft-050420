bcebe86c7cdb74c7792081c0cd0caf7dd3bd2373
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 01:53:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,9 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist = Artist.find_or_create_by(name: name)
+    if artist
+       artist.name
+    end
   end
 
   def artist_name=(name)
